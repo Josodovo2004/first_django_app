781e54a2abf712c93fb3e6f01cbb3b9c1f0cddd0
Jose Aguilar <joseaguilarsalazar2004@gmail.com>
2024-01-18 10:08:33 -0500

    Step 5 baby

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("listaAlumnos/", include("listaAlumnos.urls")),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
